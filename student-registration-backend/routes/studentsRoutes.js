1085e8dd775d4475d979c9df724f6cb0f2ddc19b
b_o_r_g_e <babalolaoluwapelumi9@gmail.com>
2025-08-04 10:03:15 +0100

    write code for student controller

--- student-registration-backend/routes/studentsRoutes.js
+++ student-registration-backend/routes/studentsRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {registerStudent} from "../controllers/studentController.js";
+import {getStudentById, getStudents, registerStudent, updateStudent} from "../controllers/studentController.js";
 
 const router = express.Router();
 
 router.post('/register', registerStudent);
+router.get('/', getStudents);
+router.get('/:matric_number', getStudentById)
+router.put('/:matric_number', updateStudent);
 
 export default router;
