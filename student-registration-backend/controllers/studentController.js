caa85eb1615acf1eb1671c43cdc47084cc4b7451
b_o_r_g_e <babalolaoluwapelumi9@gmail.com>
2025-08-04 10:03:15 +0100

    write code for student controller

--- student-registration-backend/controllers/studentController.js
+++ student-registration-backend/controllers/studentController.js
@@ -40,3 +40,75 @@ export const registerStudent = async (req, res) => {
         res.status(500).json({ error: 'Failed to register student' });
     }
 }
+
+export const getStudents = async (req, res) => {
+    try {
+        const result = await pool.query(
+            `SELECT * FROM students LIMIT 10 OFFSET 0`
+        )
+        res.status(200).json(result.rows);
+    } catch (e) {
+        console.error('Error fetching students:', e.message);
+        res.status(500).json({ error: 'Failed to get students' });
+    }
+}
+
+export const getStudentById = async (req, res) => {
+    const { matric_number } = req.params;
+
+    try {
+        const result = await pool.query(
+            `SELECT * FROM students WHERE matric_number = $1`,
+            [matric_number]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Student not found' });
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch (e) {
+        console.error('Error fetching student by ID:', e.message);
+        res.status(500).json({ error: 'Failed to get student' });
+    }
+};
+
+
+export const updateStudent = async (req, res) => {
+    const { matric_number } = req.params
+    const {
+        first_name,
+        middle_name,
+        last_name,
+        email,
+        phone_number,
+        gender,
+        date_of_birth,
+        department_id,
+        level,
+        semester,
+        session_id
+    } = req.body;
+    try {
+        const result = await pool.query(
+            `UPDATE students
+            SET first_name = $1, middle_name = $2, last_name = $3, 
+                email = $4, phone_number = $5, gender = $6, 
+                date_of_birth = $7, department_id = $8, level = $9,
+                semester = $10, session_id = $11
+                WHERE matric_number = $12
+                RETURNING *`,
+            [first_name, middle_name, last_name, email, phone_number,
+                gender, date_of_birth, department_id, level, semester,
+                session_id, matric_number]
+        )
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Student not found' });
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (e) {
+        console.error(error.message);
+        res.status(500).json({ message: "Server error"})
+    }
+}
